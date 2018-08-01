f8524f0e3e5716002586bc459dbcecab983474b1
dvekeman <dieter.vekeman@gmail.com>
2018-08-02 00:48:41 +0200

    resolver 9.3

--- src/Auth0/Internal/ApiClient.hs
+++ src/Auth0/Internal/ApiClient.hs
@@ -3,7 +3,7 @@ module Auth0.Internal.ApiClient where
 import qualified Data.ByteString as BS
 import           Network.HTTP.Client (newManager)
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
-import           Servant.Client (runClientM, mkClientEnv, ServantError, Scheme(..), BaseUrl(..))
+import           Servant.Client (runClientM, ClientEnv(..), ServantError, Scheme(..), BaseUrl(..))
 
 import Auth0.Internal.Api
 import Auth0.Internal.Types.UserInfo
@@ -11,4 +11,4 @@ import Auth0.Internal.Types.UserInfo
 getUserInfo :: BS.ByteString -> IO (Either ServantError UserInfo)
 getUserInfo token = do
   manager' <- newManager tlsManagerSettings
-  runClientM (userinfo . Just $ mkToken token) (mkClientEnv manager' (BaseUrl Https "hadruki.eu.auth0.com" 443 ""))
+  runClientM (userinfo . Just $ mkToken token) (ClientEnv manager' (BaseUrl Https "hadruki.eu.auth0.com" 443 ""))
