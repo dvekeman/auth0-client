117dfaba45c38588077d4bab1957166c506425ec
dvekeman <dieter.vekeman@gmail.com>
2018-08-02 00:16:26 +0200

    Expose ServantError

--- src/Auth0Client.hs
+++ src/Auth0Client.hs
@@ -6,8 +6,12 @@ module Auth0Client
   
   -- * /userinfo
   , UserInfo
-  , userinfo 
+  , userinfo
+   
+  -- * Servant stuff
+  , ServantError
   ) where
 
 import Auth0.Internal.Api ( Auth0Api, Token, mkToken, api, userinfo )
 import Auth0.Internal.Types.UserInfo
+import Servant.Client     ( ServantError )
